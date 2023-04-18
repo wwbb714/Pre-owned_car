08eb1ac8091c98f77f5eb972265e83ac37647e0d
wwbb714 <wwbb714@gmail.com>
2023-04-18 09:52:53 -0500

    Final

---
+++ virenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\wwbb7\onedrive\desktop\carzone\cardealerweb-django\virenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
